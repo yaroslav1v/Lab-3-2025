d950f43b63775a4be34c93e249d2114584c73c2f
Ярослав <yr.terin@gmail.com>
2025-11-12 15:03:36 +0400

    peredelal linkedlist i otchet

--- functions/LinkedListTabulatedFunction.java
+++ functions/LinkedListTabulatedFunction.java
@@ -202,19 +202,17 @@ public class LinkedListTabulatedFunction implements TabulatedFunction {
         return head.getPrev().getPoint().getX();
     }
 
-     public double getFunctionValue(double x) {
+    public double getFunctionValue(double x) {
         // Поиск интервала, содержащего x
         for (int i = 0; i < size - 1; i++) {
             FunctionNode node1 = getNodeByIndex(i);
             FunctionNode node2 = getNodeByIndex(i + 1);
 
-            double x1 = node1.getPoint().getX();
-            double x2 = node2.getPoint().getX();
-
-
-            if (x >= x1 && x <= x2) {
+            if (node1.getPoint().getX() <= x && node2.getPoint().getX() >= x) {
                 // Линейная интерполяция
+                double x1 = node1.getPoint().getX();
                 double y1 = node1.getPoint().getY();
+                double x2 = node2.getPoint().getX();
                 double y2 = node2.getPoint().getY();
 
                 return y1 + (y2 - y1) * (x - x1) / (x2 - x1);
@@ -323,5 +321,4 @@ public class LinkedListTabulatedFunction implements TabulatedFunction {
         FunctionNode newNode = addNodeByIndex(insertIndex);
         newNode.setPoint(new FunctionPoint(point));
     }
-
 }
